2e161e02e14fb73eab59c7c80fb38fddd377e3f8
Giuseppe Averta <g.averta3@gmail.com>
2024-05-10 15:19:54 +0200

    add comments

--- model/model.py
+++ model/model.py
@@ -29,11 +29,14 @@ class Model:
         self.addEdgeMode3()
 
     def buildGraphPesato(self):
+        """Questo metodo e del tutto equivalente a buildGraph, ma chiama come metodo per aggiungere gli archi "addEdgePesati" """
         self._grafo.clear()
         self._grafo.add_nodes_from(self._fermate)
         self.addEdgePesati()
 
     def addEdgePesati(self):
+        """Questo metodo assegna come peso degli edges il numero di linee che congiungono i diversi nodi.
+        """
         self._grafo.clear_edges()
         allConnessioni = DAO.getAllConnessioni()
         for c in allConnessioni:
@@ -44,7 +47,8 @@ class Model:
                 self._grafo.add_edge(self._idMap[c.id_stazP], self._idMap[c.id_stazA], weight=1)
 
     def addEdgeMode3(self):
-        #Mode 3: unica query che legge tutte le connessioni
+        """Mode 3: unica query che legge tutte le connessioni
+        """
         self._grafo.clear_edges()
         allConnessioni = DAO.getAllConnessioni()
         # print(len(allConnessioni))
@@ -53,19 +57,11 @@ class Model:
             v_nodo = self._idMap[c.id_stazA]
             self._grafo.add_edge(u_nodo, v_nodo)
             # print(f"Added edge between {u_nodo} and {v_nodo}")
-    def addEdgeMode1(self):
-        # Mode 1: doppio loop su nodi e query per ogni arco.
-        self._grafo.clear_edges()
-        for u in self._fermate:
-            for v in self._fermate:
-                res = DAO.getEdge(u,v)
-                if len(res)>0:
-                    self._grafo.add_edge(u,v)
-                    # print(f"Added edge between {u} and {v}")
-                    # return geopy.distance.geodesic(coords_1, coords_2).km
+
 
     def addEdgeMode2(self):
-        # Mode 2: loop singolo sui nodi e query per identificare i vicini
+        """Mode 2: loop singolo sui nodi e query per identificare i vicini
+        """
         self._grafo.clear_edges()
         for u in self._fermate:
             vicini = DAO.getEdgesVicini(u)
@@ -74,7 +70,20 @@ class Model:
                 self._grafo.add_edge(u,v_nodo)
                 # print(f"Added edge between {u} and {v_nodo}")
 
+    def addEdgeMode1(self):
+        """Mode 1: doppio loop su nodi e query per ogni arco.
+        """
+        self._grafo.clear_edges()
+        for u in self._fermate:
+            for v in self._fermate:
+                res = DAO.getEdge(u, v)
+                if len(res) > 0:
+                    self._grafo.add_edge(u, v)
+                    # print(f"Added edge between {u} and {v}")
+
     def getArchiPesoMaggiore(self):
+        """Print di archi con peso maggiore di 1 (agisce solo sul grafo pesato)
+        """
         if len(self._grafo.edges) == 0:
             print("Il grafo è vuoto")
             return
